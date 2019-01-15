9acf24a1d22cb4cc0385cc9682e0bfd227107fff
Twarz <cortacero.k31130@gmail.com>
2019-01-15 15:32:23 +0100

    animated chart

--- src/serial_to_mqtt.py
+++ src/serial_to_mqtt.py
@@ -18,7 +18,7 @@ def convert_light(analog_value):
     print(lux)
     return lux
 
-
+'''
 ser=serial.Serial('/dev/ttyACM0', 9600)
 client = mqtt.Client('raspy_home')
 client.connect('iot.eclipse.org')
@@ -39,3 +39,14 @@ while True:
             client.publish('/home/plant1/sensor/carbon_monoxyde', '0.5')
     time.sleep(.5)
 
+'''
+
+client = mqtt.Client('raspy_home')
+client.connect('iot.eclipse.org')
+while True:
+    client.publish('/home/plant1/sensor/moisture', '0.5')
+    client.publish('/home/plant1/sensor/temperature', '0.5')
+    client.publish('/home/plant1/sensor/luminosity1', '0.5')
+    client.publish('/home/plant1/sensor/luminosity2', '0.5')
+    client.publish('/home/plant1/sensor/carbon_monoxyde', '0.5')
+    time.sleep(.5)
--- webapp/static/graph.js
+++ webapp/static/graph.js
@@ -1,32 +1,57 @@
-var ctx = document.getElementById("line-chart-water").getContext('2d');
-var myChart = new Chart(ctx, {
+var ctxw = document.getElementById("line-chart-water").getContext('2d');
+var water_dataa = [];
+var water_chart = new Chart(ctxw, {
+  type: 'line',
+  data: {
+    labels: [],
+    datasets: [{ 
+        data: water_dataa,
+        label: "Africa",
+        borderColor: "#3e95cd",
+        fillColor : 'rgba(62, 149, 205, 0.5)'
+      }
+    ]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'World population per region (in millions)'
+    }
+  }
+});
+
+var ctxl = document.getElementById("line-chart-light").getContext('2d');
+var water_data = [86,114,106,106,107,111,133,221,783,2478];
+var myChart = new Chart(ctxl, {
   type: 'line',
   data: {
     labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
     datasets: [{ 
-        data: [86,114,106,106,107,111,133,221,783,2478],
+        data: water_data,
         label: "Africa",
         borderColor: "#3e95cd",
-        fill: false
-      }, { 
-        data: [282,350,411,502,635,809,947,1402,3700,5267],
-        label: "Asia",
-        borderColor: "#8e5ea2",
-        fill: false
-      }, { 
-        data: [168,170,178,190,203,276,408,547,675,734],
-        label: "Europe",
-        borderColor: "#3cba9f",
-        fill: false
-      }, { 
-        data: [40,20,10,16,24,38,74,167,508,784],
-        label: "Latin America",
-        borderColor: "#e8c3b9",
-        fill: false
-      }, { 
-        data: [6,3,2,2,7,26,82,172,312,433],
-        label: "North America",
-        borderColor: "#c45850",
+        fillColor : 'rgba(62, 149, 205, 0.5)'
+      }
+    ]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'World population per region (in millions)'
+    }
+  }
+});
+
+var ctxg = document.getElementById("line-chart-gaz").getContext('2d');
+var water_data = [86,114,106,106,107,111,133,221,783,2478];
+var myChart = new Chart(ctxg, {
+  type: 'line',
+  data: {
+    labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
+    datasets: [{ 
+        data: water_data,
+        label: "Africa",
+        borderColor: "#3e95cd",
         fill: false
       }
     ]
@@ -38,3 +63,72 @@ var myChart = new Chart(ctx, {
     }
   }
 });
+
+var ctxt = document.getElementById("line-chart-temperature").getContext('2d');
+var water_data = [86,114,106,106,107,111,133,221,783,2478];
+var myChart = new Chart(ctxt, {
+  type: 'line',
+  data: {
+    labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
+    datasets: [{ 
+        data: water_data,
+        label: "Africa",
+        borderColor: "#3e95cd",
+        fill: false
+      }
+    ]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'World population per region (in millions)'
+    }
+  }
+});
+
+
+client = new Paho.MQTT.Client("ws://iot.eclipse.org:80/ws", "javascript-client");
+
+// set callback handlers
+client.onConnectionLost = onConnectionLost;
+client.onMessageArrived = onMessageArrived;
+
+// connect the client
+client.connect({onSuccess:onConnect});
+
+
+// called when the client connects
+function onConnect() {
+  // Once a connection has been made, make a subscription and send a message.
+  console.log("onConnect");
+  client.subscribe("/home/plant1/sensor/moisture");
+}
+
+// called when the client loses its connection
+function onConnectionLost(responseObject) {
+  if (responseObject.errorCode !== 0) {
+    console.log("onConnectionLost:"+responseObject.errorMessage);
+  }
+}
+
+var xVal = 0;
+var yVal = 100; 
+var updateInterval = 1000;
+var dataLength = 50; // number of dataPoints visible at any point
+
+// called when a message arrives
+function onMessageArrived(message) {
+  console.log("onMessageArrived:"+message.payloadString);
+  yVal = parseFloat(message.payloadString);
+  water_dataa.push(yVal);
+  water_chart.data.labels.push(xVal)
+  xVal++;
+  console.log(water_dataa);
+
+	if (water_dataa.length > dataLength) {
+    water_chart.data.labels.shift()
+		water_dataa.shift();
+	}
+
+	water_chart.update();
+}
