86848191b4f1900df6de0cb867d13e3011a008af
haze-sama <haze-sama@live.fr>
2019-01-16 13:24:47 +0100

    update: adding buffer

--- src/serial_to_mqtt.py
+++ src/serial_to_mqtt.py
@@ -4,6 +4,7 @@ import time
 from math import pow
 from collections import deque
 from random import random
+import numpy as np
 
 
 def convert_temperature(analog_value):
@@ -19,35 +20,73 @@ def convert_light(analog_value):
     print(lux)
     return lux
 
+def convert_gas(analog_value):
+    return analog_value/1023.0
+
+def convert_moisture(analog_value):
+    return analog_value/1023.0
+
+def get_mean_deque(buffer):
+    return np.mean((list(buffer)))
+
+
+
+buffer_temperature = deque(maxlen=10)
+
+buffer_light_1 = deque(maxlen=10)
+buffer_light_2 = deque(maxlen=10)
+
+buffer_gas = deque(maxlen=10)
+
+buffer_moisture = deque(maxlen=10)
+
 '''
 ser=serial.Serial('/dev/ttyACM0', 9600)
 client = mqtt.Client('raspy_home')
 client.connect('iot.eclipse.org')
 
-temp_buffer1 = deque(maxlen=100)
-temp_buffer1 = deque(maxlen=100)
-temp_buffer1 = deque(maxlen=100)
+buffer_temperature = deque(maxlen=10)
+
+buffer_light_1 = deque(maxlen=10)
+buffer_light_2 = deque(maxlen=10)
+
+buffer_gas = deque(maxlen=10)
+
+buffer_moisture = deque(maxlen=10)
 
 while True:
     if(ser.in_waiting>0):
         line=ser.readline()
         analog_values=line.split(':')
         if len(analog_values) == 5:
-            client.publish('/home/plant1/sensor/moisture', '0.5')
-            client.publish('/home/plant1/sensor/temperature', convert_temperature(int(analog_values[4])))
-            client.publish('/home/plant1/sensor/luminosity1', convert_light(int(analog_values[0])))
-            client.publish('/home/plant1/sensor/luminosity2', convert_light(int(analog_values[1])))
-            client.publish('/home/plant1/sensor/carbon_monoxyde', '0.5')
-    time.sleep(.5)
+            buffer_light_1.append(int(analog_values[0]))
+            buffer_light_2.append(int(analog_values[1]))
+            buffer_gas.append(int(analog_values[2]))
+            buffer_moisture.append(int(analog_values[3]))
+            buffer_temperature.append(int(analog_values[4]))
 
+            client.publish('/home/plant1/sensor/moisture', convert_moisture(get_mean_deque(buffer_moisture)))
+            client.publish('/home/plant1/sensor/temperature', convert_temperature(get_mean_deque(buffer_temperature)))
+            client.publish('/home/plant1/sensor/luminosity1', convert_temperature(get_mean_deque(buffer_light_1)))
+            client.publish('/home/plant1/sensor/luminosity2', convert_temperature(get_mean_deque(buffer_light_2)))
+            client.publish('/home/plant1/sensor/carbon_monoxyde', convert_gas(get_mean_deque(buffer_gas)))
+
+    time.sleep(.5)
 '''
+
 
 client = mqtt.Client('raspy_home')
 client.connect('iot.eclipse.org')
 while True:
-    client.publish('/home/plant1/sensor/moisture', 0.5 + random()*0.05)
-    client.publish('/home/plant1/sensor/temperature', 14 + random())
-    client.publish('/home/plant1/sensor/luminosity1', 25 + 3*random())
-    client.publish('/home/plant1/sensor/luminosity2', 18 + 5*random())
-    client.publish('/home/plant1/sensor/carbon_monoxyde', 0.5 + random()*0.05)
+    buffer_light_1.append(450 + 10*random())
+    buffer_light_2.append(400 + 10*random())
+    buffer_gas.append(0.8 + 0.01*random())
+    buffer_moisture.append(0.4 + 0.01*random())
+    buffer_temperature.append(12 + 2*random())
+
+    client.publish('/home/plant1/sensor/moisture', convert_moisture(get_mean_deque(buffer_moisture)))
+    client.publish('/home/plant1/sensor/temperature', convert_temperature(get_mean_deque(buffer_temperature)))
+    client.publish('/home/plant1/sensor/luminosity1', convert_temperature(get_mean_deque(buffer_light_1)))
+    client.publish('/home/plant1/sensor/luminosity2', convert_temperature(get_mean_deque(buffer_light_2)))
+    client.publish('/home/plant1/sensor/carbon_monoxyde', convert_gas(get_mean_deque(buffer_gas)))
     time.sleep(1)
